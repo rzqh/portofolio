7944d75a2698d6e6b6d699c2cba410ee69e5c969
rzqh <rizqih@protonmail.ch>
2025-03-24 00:35:33 +0700

    fix: correct ' escaped to &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,7 +231,7 @@ export default function Page() {
                     Check out my latest work
                   </h2>
                   <p className='text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'>                    
-                    I've worked on a variety of projects, from simple AI chatbot to image recognition and recommendation system model. Here are a few of my favorites.
+                    I&apos;ve worked on a variety of projects, from simple AI chatbot to image recognition and recommendation system model. Here are a few of my favorites.
                   </p>
                 </div>
               </div>
